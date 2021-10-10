cd027842e54b6384e993adab9f29aa540c31fc05
Murilo Gonçalves <murilo.gon@live.com>
2021-10-10 08:11:56 -0300

    feat: create Todo view model

---
+++ Models/Todo.cs
@@ -0,0 +1,9 @@
+namespace DotnetTodoMvc.Models
+{
+  public class Todo
+  {
+    public int Id { get; set; }
+    public string TaskName { get; set; }
+    public bool IsComplete { get; set; }
+  }
+}
